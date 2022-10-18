5dca52e5c8130c4f03f4f5a673f2dca2f8301936
Ravkeerat02 <42888466+Ravkeerat02@users.noreply.github.com>
2022-10-18 02:48:19 -0400

    Complete - added comment(explanation)

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ const mongoose = require('mongoose');
 
 const employeeRouter = require('./routes/employeeRoute');
 const userRouter = require('./routes/userRoute');
-const SERVER_PORT = 4000
 
+const SERVER_PORT = 4000 //port numberer initialized
+
+//DB connection string
 const DB_URL = "mongodb+srv://RK_02:crazyShit@cluster0.iu4uasl.mongodb.net/assignment_db?retryWrites=true&w=majority"
 
 const app = express();
@@ -25,15 +27,15 @@ mongoose.connect(DB_URL, {
     process.exit();
 });
 
-app.use("/api/emp/",employeeRouter)
+app.use("/api/emp/",employeeRouter) //For the employee page
 
-app.use("/api/user/",userRouter)
+app.use("/api/user/",userRouter) // For the user page
 
 app.route("/").get((req,res)=>{
     res.send("<h1>Assignment 1 - MongoDb using NodeJS and Express</h1>");
 })
     
- 
+//Refers to the port beign used 
 app.listen(SERVER_PORT, () =>{
     console.log(`Server running at http://localhost:${SERVER_PORT}/`)
 })
